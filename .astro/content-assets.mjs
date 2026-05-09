4383b50e6858bab8c8796d22153cab71942065ae
Conan-jine <2940119526@qq.com>
2026-05-09 23:17:41 +0800

    chore: gitignore 清理 .astro 缓存

--- .astro/content-assets.mjs
+++
@@ -1,6 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z4kqOs from "fireworks.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FHexo-Fluid%E7%BE%8E%E5%8C%96%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_Z1tisbR from "礼花演示.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FHexo-Fluid%E7%BE%8E%E5%8C%96%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_Z2rIq3a from "分区情况图.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FLinux-From-Scratch-11-3-%E5%86%8D%E5%AE%9E%E8%B7%B5%2Findex.md";
-export default new Map([["fireworks.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FHexo-Fluid%E7%BE%8E%E5%8C%96%2Findex.md", __ASTRO_IMAGE_IMPORT_Z4kqOs], ["礼花演示.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FHexo-Fluid%E7%BE%8E%E5%8C%96%2Findex.md", __ASTRO_IMAGE_IMPORT_Z1tisbR], ["分区情况图.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2FLinux-From-Scratch-11-3-%E5%86%8D%E5%AE%9E%E8%B7%B5%2Findex.md", __ASTRO_IMAGE_IMPORT_Z2rIq3a]]);
-		
